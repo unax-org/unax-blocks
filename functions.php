e0e92caa8d8c9632148620b284e96bebc523708f
user <29710963+atanasantonov@users.noreply.github.com>
2021-12-06 13:53:53 +0200

    Basic styling added

--- functions.php
+++ functions.php
@@ -15,11 +15,11 @@ if ( ! function_exists( 'unax_blocks_support' ) ) :
 		// Add support for editor styles.
 		add_theme_support( 'editor-styles' );
 
-		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
-
 		// Add support for custom units.
 		add_theme_support( 'custom-units' );
+
+		// Enqueue editor styles.
+		add_editor_style( 'dist/editor-style.css' );
 	}
 	add_action( 'after_setup_theme', 'unax_blocks_support' );
 endif;
@@ -28,8 +28,12 @@ endif;
  * Enqueue scripts and styles.
  */
 function unax_blocks_scripts() {
+	// Enqueue normalize.
+	wp_enqueue_style( 'normalize', get_template_directory_uri() . '/assets/css/normalize.css', array(), '8.0.1' );
+
 	// Enqueue theme stylesheet.
-	wp_enqueue_style( 'unax-blocks-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'unax-blocks-style', get_template_directory_uri() . '/style.css', array( 'normalize' ), wp_get_theme()->get( 'Version' ) );
+
 }
 
 add_action( 'wp_enqueue_scripts', 'unax_blocks_scripts' );
