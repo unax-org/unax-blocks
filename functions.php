ac1a724e9a0a76573a33759e6d77e5d279dc250f
user <29710963+atanasantonov@users.noreply.github.com>
2021-12-07 12:46:49 +0200

    Edit: color palette

--- functions.php
+++ functions.php
@@ -5,6 +5,5 @@
  * @package Unax Blocks
  */
 
-require_once get_template_directory() . '/inc/customizer.php';
 require_once get_template_directory() . '/inc/template-functions.php';
 require_once get_template_directory() . '/inc/template-hooks.php';
